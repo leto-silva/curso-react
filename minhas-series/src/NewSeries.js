e58b6be8efeeae6373f7348f1a049df19efb7854
wellington <wellingtoneugenio@gmail.com>
2018-12-01 01:24:22 -0300

    terminando...mapeamento via router

---
+++ minhas-series/src/NewSeries.js
@@ -0,0 +1,71 @@
+import React,{ Component } from 'react';
+
+import api from './Api';
+
+
+const statuses = {
+    'Watched': 'Assistido',
+    'Watching': 'Assistindo',
+    'Towatch': 'Assistir'
+}
+
+
+class NewSeries extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+          genres: [],
+          isLoading: false
+        };
+      }
+    
+      componentDidMount(){
+       this.setState({isLoading: true});
+       api.loadGenres()
+        .then( (res) => {
+          this.setState({
+            isLoading: false,
+            genres: res.data
+          })
+    
+        } );
+      }
+    
+      renderGenreLink(genre){
+        return(
+          <span>&nbsp;<a href>{genre}</a>&nbsp;</span>
+        )
+    
+      }
+
+    render() {
+        return( 
+          <section className="intro-section">
+            <h1>Nova Série</h1>
+            <form>
+                Nome:< input type='Text' className='form-control'></input><br />
+                Status:
+                <select >
+                    { Object.keys(statuses)
+                        .map( key => <option key={key} value={key}>{statuses[key]} </option>)}
+                </select> <br />
+                Gêneros:
+                <select >
+                    { this.state.genres
+                        .map( key => <option key={key} value={key}>{key} </option>)}
+                </select> <br />           
+                Comentários: <textarea className='form-control'></textarea><br />
+            </form>
+            
+            
+          </section> )
+
+
+
+        
+
+    }
+
+}
+
+export default NewSeries
