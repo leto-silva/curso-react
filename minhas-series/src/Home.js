5bfb8ece7b727dd8ab83f2a4b659c64423d20606
wellington <wellingtoneugenio@gmail.com>
2018-12-03 15:34:44 -0300

    comite

---
+++ minhas-series/src/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+
+
+class Home extends Component {
+
+    render(){
+        return(      
+            <div>
+                <section id="intro" className="intro-section">
+                    <div className="container">
+                                    <div className="row">
+                                        <div className="col-lg-12">
+                                        <h1><img src="images/logo.png" /></h1>
+                                        <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+                                        </div>
+                                    </div>
+                            </div>
+                        </section>
+
+                        <section>
+                            {
+                            this.isLoading &&
+                                <span>Aguarde carregando...</span>
+                            }
+                            { !this.isLoading &&
+                                <div>
+                                Ver séries do gênero:
+                                    {/*this.state.genres.map(this.renderGenreLink)*/}
+                                </div>  
+                            }   
+                        </section>
+                    </div>
+                )
+    }    
+}
+export default Home
